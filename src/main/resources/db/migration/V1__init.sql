676deaf8f17074c98faec6f9312e3ed2262d8433
Alexandre Belisário Duarte Leite de Andrade <alex_dev@outlook.com.br>
2026-02-28 23:11:30 -0300

    feat(payment-service): bootstrap inicial com infraestrutura e migração Flyway
    - Configura Spring Boot 4 + Java 21
    - Adiciona Docker Compose (Postgres, Kafka, Zookeeper, Kafka UI)
    - Configura Flyway e cria V1__init.sql
    - Valida migração do banco de dados
    - Configura JPA + Hikari

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,10 @@
+create table payments (
+                          id uuid primary key,
+                          order_id uuid not null,
+                          status varchar(30) not null,
+                          amount numeric(19,2) not null,
+                          created_at timestamptz not null,
+                          updated_at timestamptz not null
+);
+
+create unique index uk_payments_order_id on payments(order_id);
