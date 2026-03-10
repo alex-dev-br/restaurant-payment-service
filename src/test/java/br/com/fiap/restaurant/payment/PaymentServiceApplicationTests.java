16e5795389d6e7c47904934999337d112babe62b
Alexandre Belisário Duarte Leite de Andrade <alex_dev@outlook.com.br>
2026-03-09 21:15:21 -0300

    ci: add RabbitMQ service and stabilize application context test

--- src/test/java/br/com/fiap/restaurant/payment/PaymentServiceApplicationTests.java
+++ src/test/java/br/com/fiap/restaurant/payment/PaymentServiceApplicationTests.java
@@ -3,7 +3,10 @@ package br.com.fiap.restaurant.payment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.rabbitmq.listener.simple.auto-startup=false",
+		"spring.task.scheduling.enabled=false"
+})
 class PaymentServiceApplicationTests {
 
 	@Test
