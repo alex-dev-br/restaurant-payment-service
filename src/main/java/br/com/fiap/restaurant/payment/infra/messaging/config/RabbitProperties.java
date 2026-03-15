3ce2fc8cdeed574b1f6f7ebe16150c009cbbf987
Alexandre Belisário Duarte Leite de Andrade <alex_dev@outlook.com.br>
2026-03-15 17:54:22 -0300

    refactor: adapta rabbit properties para estrutura hierárquica

--- src/main/java/br/com/fiap/restaurant/payment/infra/messaging/config/RabbitProperties.java
+++ src/main/java/br/com/fiap/restaurant/payment/infra/messaging/config/RabbitProperties.java
@@ -5,76 +5,151 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "app.rabbit")
 public class RabbitProperties {
 
-    private String orderExchange;
-    private String paymentExchange;
-    private String orderCreatedQueue;
-    private String orderCreatedRoutingKey;
-    private String paymentApprovedRoutingKey;
-    private String paymentPendingRoutingKey;
-    private String approvedDebugQueue;
-    private String pendingDebugQueue;
+    private Exchange exchange = new Exchange();
+    private RoutingKey routingKey = new RoutingKey();
+    private QueueConfig queue = new QueueConfig();
+    private Dlq dlq = new Dlq();
 
-    public String getOrderExchange() {
-        return orderExchange;
+    public Exchange getExchange() {
+        return exchange;
     }
 
-    public void setOrderExchange(String orderExchange) {
-        this.orderExchange = orderExchange;
+    public void setExchange(Exchange exchange) {
+        this.exchange = exchange;
     }
 
-    public String getPaymentExchange() {
-        return paymentExchange;
+    public RoutingKey getRoutingKey() {
+        return routingKey;
     }
 
-    public void setPaymentExchange(String paymentExchange) {
-        this.paymentExchange = paymentExchange;
+    public void setRoutingKey(RoutingKey routingKey) {
+        this.routingKey = routingKey;
     }
 
-    public String getOrderCreatedQueue() {
-        return orderCreatedQueue;
+    public QueueConfig getQueue() {
+        return queue;
     }
 
-    public void setOrderCreatedQueue(String orderCreatedQueue) {
-        this.orderCreatedQueue = orderCreatedQueue;
+    public void setQueue(QueueConfig queue) {
+        this.queue = queue;
     }
 
-    public String getOrderCreatedRoutingKey() {
-        return orderCreatedRoutingKey;
+    public Dlq getDlq() {
+        return dlq;
     }
 
-    public void setOrderCreatedRoutingKey(String orderCreatedRoutingKey) {
-        this.orderCreatedRoutingKey = orderCreatedRoutingKey;
+    public void setDlq(Dlq dlq) {
+        this.dlq = dlq;
     }
 
-    public String getPaymentApprovedRoutingKey() {
-        return paymentApprovedRoutingKey;
+    public static class Exchange {
+        private String order;
+        private String payment;
+
+        public String getOrder() {
+            return order;
+        }
+
+        public void setOrder(String order) {
+            this.order = order;
+        }
+
+        public String getPayment() {
+            return payment;
+        }
+
+        public void setPayment(String payment) {
+            this.payment = payment;
+        }
     }
 
-    public void setPaymentApprovedRoutingKey(String paymentApprovedRoutingKey) {
-        this.paymentApprovedRoutingKey = paymentApprovedRoutingKey;
+    public static class RoutingKey {
+        private String orderCreated;
+        private String paymentApproved;
+        private String paymentPending;
+
+        public String getOrderCreated() {
+            return orderCreated;
+        }
+
+        public void setOrderCreated(String orderCreated) {
+            this.orderCreated = orderCreated;
+        }
+
+        public String getPaymentApproved() {
+            return paymentApproved;
+        }
+
+        public void setPaymentApproved(String paymentApproved) {
+            this.paymentApproved = paymentApproved;
+        }
+
+        public String getPaymentPending() {
+            return paymentPending;
+        }
+
+        public void setPaymentPending(String paymentPending) {
+            this.paymentPending = paymentPending;
+        }
     }
 
-    public String getPaymentPendingRoutingKey() {
-        return paymentPendingRoutingKey;
+    public static class QueueConfig {
+        private String paymentOrderCreated;
+        private String paymentApprovedDebug;
+        private String paymentPendingDebug;
+
+        public String getPaymentOrderCreated() {
+            return paymentOrderCreated;
+        }
+
+        public void setPaymentOrderCreated(String paymentOrderCreated) {
+            this.paymentOrderCreated = paymentOrderCreated;
+        }
+
+        public String getPaymentApprovedDebug() {
+            return paymentApprovedDebug;
+        }
+
+        public void setPaymentApprovedDebug(String paymentApprovedDebug) {
+            this.paymentApprovedDebug = paymentApprovedDebug;
+        }
+
+        public String getPaymentPendingDebug() {
+            return paymentPendingDebug;
+        }
+
+        public void setPaymentPendingDebug(String paymentPendingDebug) {
+            this.paymentPendingDebug = paymentPendingDebug;
+        }
     }
 
-    public void setPaymentPendingRoutingKey(String paymentPendingRoutingKey) {
-        this.paymentPendingRoutingKey = paymentPendingRoutingKey;
-    }
+    public static class Dlq {
+        private String paymentOrderCreated;
+        private String paymentApprovedDebug;
+        private String paymentPendingDebug;
 
-    public String getApprovedDebugQueue() {
-        return approvedDebugQueue;
-    }
+        public String getPaymentOrderCreated() {
+            return paymentOrderCreated;
+        }
 
-    public void setApprovedDebugQueue(String approvedDebugQueue) {
-        this.approvedDebugQueue = approvedDebugQueue;
-    }
+        public void setPaymentOrderCreated(String paymentOrderCreated) {
+            this.paymentOrderCreated = paymentOrderCreated;
+        }
 
-    public String getPendingDebugQueue() {
-        return pendingDebugQueue;
-    }
+        public String getPaymentApprovedDebug() {
+            return paymentApprovedDebug;
+        }
 
-    public void setPendingDebugQueue(String pendingDebugQueue) {
-        this.pendingDebugQueue = pendingDebugQueue;
+        public void setPaymentApprovedDebug(String paymentApprovedDebug) {
+            this.paymentApprovedDebug = paymentApprovedDebug;
+        }
+
+        public String getPaymentPendingDebug() {
+            return paymentPendingDebug;
+        }
+
+        public void setPaymentPendingDebug(String paymentPendingDebug) {
+            this.paymentPendingDebug = paymentPendingDebug;
+        }
     }
 }
